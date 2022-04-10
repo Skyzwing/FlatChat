c09fac326f9005a238510520ec6e8586ca0ddb2d
surachet.y <surachet.y@creativeme.co.th>
2022-04-10 15:30:08 +0700

    Initial Commit

---
+++ FlatChat/FlatChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlatChatApp.swift
+//  FlatChat
+//
+//  Created by Surachet Yaitammasan on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct FlatChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
